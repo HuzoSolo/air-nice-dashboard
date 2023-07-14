a0d73ea37b785436c6db0c5b31eca23b0f264816
Huzeyfe-Haydesoft <muhuzhanguzel@gmail.com>
2023-07-14 12:54:59 +0300

    folder added

---
+++ assets/js/list.js
@@ -0,0 +1,13 @@
+function toggleActive(element){
+
+    if(element.classList.contains("active")){
+
+        element.classList.remove("active")
+
+    }else{
+
+        element.classList.add("active")
+
+    }
+
+}
